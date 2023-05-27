1b7bf2f1d1ad45222dc81c7064732e3683d54382
dilipsanapinb <dilipinbsanap@gmail.com>
2023-05-27 14:07:58 +0530

    Backend folderdatad

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
+server.get("/", (req, res) => {
+  res.send("Welcome to Freelancers Service")
+})
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
